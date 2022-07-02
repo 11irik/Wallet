1e569ada0775a01549ba1e1149b9c947e16aa013
Kirill Lukashin <lykashin.kirill@gmail.com>
2022-07-02 20:03:45 +0400

    .

--- frontend/src/api/account.js
+++ frontend/src/api/account.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 
 const accounts = Vue.resource('/account{/id}');
 
+
 export default {
     add: account => accounts.save({}, account),
     update: account => accounts.update({id: account.id}, account),
